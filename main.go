844184b3f58ef0be57f13610583c573e55994547
Mifthahuddin <2109116098@student.unmul.ac.id>
2024-05-18 22:22:53 +0800

    update

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	report2 "forest/usecases/report"
 	"forest/usecases/user"
 	"log"
+	"os"
 
 	"github.com/labstack/echo/v4"
 )
@@ -61,6 +62,6 @@ func main() {
 	r.GET("/reports/:id", reportHandler.GetReportByID)
 	r.DELETE("/reports/:id", reportHandler.DeleteReport)
 
-	e.Logger.Fatal(e.Start(":8000"))
+	e.Logger.Fatal(e.Start(os.Getenv("PORT")))
 
 }
